cc0623e86cb63a459fa633cf810a1ee1a1b97482
Muhammad Taqi Hassan <=>
2019-10-22 16:31:33 +0500

    change from feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> dasdasd asd asdaasasd and save to reload.
+          Edit <code>src/App.js</code> dasdasd asd asdaasasd and save to reload. Messages chage from new branch features
         </p>
         <a
           className="App-link"
